086fb9a8c1b5b5bc58be448bd0c1486e60cff853
Xaviour Greenhalgh <xaviour@squareiq.com.au>
2024-02-01 17:46:40 +1000

    feat: eslint setup

--- src/api/models/stats/index.ts
+++ src/api/models/stats/index.ts
@@ -1,17 +1,17 @@
 export enum CharacterStat {
-  Strength= 'strength',
-    Charisma= 'charisma',
-    Dexterity= 'dexterity',
-    Wisdom= 'wisdom',
-    Intelligence= 'intelligence',
-    Constitution= 'constitution',
+  Strength = 'strength',
+  Charisma = 'charisma',
+  Dexterity = 'dexterity',
+  Wisdom = 'wisdom',
+  Intelligence = 'intelligence',
+  Constitution = 'constitution'
 }
 
 export interface CharacterStatArray {
-  [CharacterStat.Strength]: number,
-  [CharacterStat.Charisma]: number,
-  [CharacterStat.Dexterity]: number,
-  [CharacterStat.Wisdom]: number,
-  [CharacterStat.Intelligence]: number,
-  [CharacterStat.Constitution]: number,
+  [CharacterStat.Strength]: number;
+  [CharacterStat.Charisma]: number;
+  [CharacterStat.Dexterity]: number;
+  [CharacterStat.Wisdom]: number;
+  [CharacterStat.Intelligence]: number;
+  [CharacterStat.Constitution]: number;
 }
